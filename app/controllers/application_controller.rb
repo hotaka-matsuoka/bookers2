f13a98d469b21f450c5d5ee354ac1ffe9aada2d1
hotaka-matsuoka <matsuhota_mail108@icloud.com>
2020-06-29 18:31:11 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
-  protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  def after_sign_out_path_for(resource)
+    root_path
   end
 
-  private 
-  def after_sign_out_path_for(resource)
-    top_path
+  def after_sign_in_path_for(resource)
+    books_path
   end
+
+  def after_sign_up_path_for(resource)
+    books_path
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+  end
+
 end
